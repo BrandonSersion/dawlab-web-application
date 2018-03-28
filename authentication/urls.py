9a7a199e5633293573ca03929ebb344775e4019e
BrandonSersion <addingavocado@gmail.com>
2018-03-28 00:18:23 -0400

    Set up private employee_zone that requires user authentication.

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,4 +5,5 @@ from authentication import views
 urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
     url(r'^$', views.authentication_page, name='authentication'),
+    url(r'^employee_zone/', views.employee_zone_page, name='employee_zone'),
 ]
