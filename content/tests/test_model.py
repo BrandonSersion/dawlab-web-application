400e34530d39b8a6a983af72db60fc52adae73fc
BrandonSersion <addingavocado@gmail.com>
2018-04-15 21:34:36 -0400

    Refactor templates and test_models

--- content/tests/test_model.py
+++ content/tests/test_model.py
@@ -5,72 +5,229 @@ from unittest import skip
 
 class EmployeeModelTest(TestCase):
 
-    def test_default_text(self):
-        employee = Employee()
-        self.assertEqual(employee.last_name, '')
-        self.assertEqual(employee.first_name, '')
-        self.assertEqual(employee.job_title, '')
-        self.assertEqual(employee.job_description, '')
-        self.assertEqual(employee.bio, '')
-        self.assertEqual(employee.skills, '')
+    # Global test constants used for the Employee class.
+    FIRST_NAME = 'Charles'
+    LAST_NAME = 'Bronson'
+    JOB_TITLE = 'Agent'
+    JOB DESCRIPTION = 'Tough Guy'
+    SKILLS = 'Fighting Crime'
+    BIO = ('He starred in films such as Once Upon a Time in the West, The Magnificent Seven, The Dirty Dozen,'
+    ' The Great Escape, Battle of the Bulge, Rider on the Rain, The Mechanic, and the Death Wish series.'
+    ' He was often cast in the role of a police officer, gunfighter, or vigilante in a revenge-oriented plot.')
+    EMPLOYEE = Employee(
+            first_name = FIRST_NAME,
+            last_name = LAST_NAME,
+            job_title = JOB_TITLE,
+            job_description = JOB_DESCRIPTION,
+            skills = SKILLS, 
+            bio = BIO
+    )
 
-    def test_employee_lookup_default_first_name_last_name(self):
-        employee = Employee(first_name = 'Charles', last_name = 'Bronson', job_title = 'Agent', job_description = 'Tough guy.', skills='Fighting crime.', bio='Steven Frederic Seagal (/sɪˈɡɑːl/; born April 10, 1952) is an American actor, producer, screenwriter, director, martial artist, and musician who holds American, Russian, and Serbian citizenship.')
-        employee.full_clean()
-        
-        employee.save()
-        self.assertEqual(employee.__str__(), 'Charles Bronson')
+    def test_default_text_is_null(self):
+        self.assertEqual(EMPLOYEE.last_name, '')
+        self.assertEqual(EMPLOYEE.first_name, '')
+        self.assertEqual(EMPLOYEE.job_title, '')
+        self.assertEqual(EMPLOYEE.job_description, '')
+        self.assertEqual(EMPLOYEE.bio, '')
+        self.assertEqual(EMPLOYEE.skills, '')
 
     def test_can_insert_data(self):
-        employee = Employee(first_name = 'Paul', last_name = 'Blart', job_title = 'Mall Cop', job_description = 'The hero of the story.', skills='Fighting crime.', bio='Paul Blart: Mall Cop is a 2009 American action comedy film co-written by Kevin James, who stars as the title character, Paul Blart. Filming began in February 2008 with most of the shooting taking place at the Burlington Mall in Burlington, Massachusetts.')
-        self.assertEqual(employee.first_name, 'Paul')
-        self.assertEqual(employee.last_name, 'Blart')
-        self.assertEqual(employee.job_title, 'Mall Cop')
-        self.assertEqual(employee.job_description, 'The hero of the story.')
-        self.assertEqual(employee.skills, 'Fighting crime.')
-        self.assertEqual(employee.bio, 'Paul Blart: Mall Cop is a 2009 American action comedy film co-written by Kevin James, who stars as the title character, Paul Blart. Filming began in February 2008 with most of the shooting taking place at the Burlington Mall in Burlington, Massachusetts.')
-        
+        EMPLOYEE.full_clean()
+        EMPLOYEE.save()
+
+        self.assertEqual(EMPLOYEE.first_name, FIRST_NAME)
+        self.assertEqual(EMPLOYEE.last_name, LAST_NAME)
+        self.assertEqual(EMPLOYEE.job_title, JOB_TITLE)
+        self.assertEqual(EMPLOYEE.job_description, JOB_DESCRIPTION)
+        self.assertEqual(EMPLOYEE.skills, SKILLS)
+        self.assertEqual(EMPLOYEE.bio, BIO)
+
+    def test_employee_lookup_prints_first_name_last_name(self):
+        employee = Employee(
+            first_name = FIRST_NAME, 
+            last_name = LAST_NAME, 
+            job_title =  JOB_TITLE, 
+            job_description = JOB DESCRIPTION, 
+            skills = SKILLS, 
+            bio = BIO,
+        )
         employee.full_clean()
         employee.save()
+        results = employee.__str__()
 
-    def test_duplicate_employee_names_are_invalid(self):
-        employee_1 = Employee(first_name = 'Charles', last_name = 'Bronson', job_title = 'Agent', job_description = 'Tough guy.', skills='Fighting crime.', bio='Steven Frederic Seagal (/sɪˈɡɑːl/; born April 10, 1952) is an American actor, producer, screenwriter, director, martial artist, and musician who holds American, Russian, and Serbian citizenship.')
-        employee_2 = Employee(first_name = 'Charles', last_name = 'Bronson', job_title = 'This', job_description = 'text', skills='differs', bio='.')
+        expected = "Charles Bronson"
+        self.assertEqual(results, expected)
+        
+    def test_duplicate_employee_name_is_invalid(self):
+        employee_1 = Employee(
+            first_name = FIRST_NAME,
+            last_name = LAST_NAME,
+            job_title = JOB_TITLE,
+            job_description = JOB_DESCRIPTION,
+            skills = SKILLS, 
+            bio = BIO
+        )
         employee_1.full_clean()
         employee_1.save()
+        employee_2 = Employee(
+            first_name = FIRST_NAME,
+            last_name = LAST_NAME,
+            job_title = 'These',
+            job_description = 'strings',
+            skills = 'can',
+            bio = 'differ.'
+        )
 
         with self.assertRaises(ValidationError):
             employee_2.full_clean()
+            employee_2.save()
 
-    def test_similar_employee_names_are_valid(self):
-        employee_1 = Employee(first_name = 'Charles', last_name = 'Bronson', job_title = 'Agent', job_description = 'Tough guy.', skills='Fighting crime.', bio='Steven Frederic Seagal (/sɪˈɡɑːl/; born April 10, 1952) is an American actor, producer, screenwriter, director, martial artist, and musician who holds American, Russian, and Serbian citizenship.')
-        employee_2 = Employee(first_name = 'Charles', last_name = 'Pastor', job_title = 'Agent', job_description = 'Tough guy.', skills='Fighting crime.', bio='Steven Frederic Seagal (/sɪˈɡɑːl/; born April 10, 1952) is an American actor, producer, screenwriter, director, martial artist, and musician who holds American, Russian, and Serbian citizenship.')
-        employee_3 = Employee(first_name = 'Eddy', last_name = 'Bronson', job_title = 'Agent', job_description = 'Tough guy.', skills='Fighting crime.', bio='Steven Frederic Seagal (/sɪˈɡɑːl/; born April 10, 1952) is an American actor, producer, screenwriter, director, martial artist, and musician who holds American, Russian, and Serbian citizenship.')
-        
+    def test_duplicate_first_name_different_last_name_is_valid(self):
+        duplicate_first_name = FIRST_NAME 
+
+        employee_1 = Employee(
+            first_name = duplicate_first_name,
+            last_name = LAST_NAME,
+            job_title = JOB_TITLE,
+            job_description = JOB_DESCRIPTION,
+            skills = SKILLS,
+            bio = BIO
+        )
         employee_1.full_clean()
         employee_1.save()
+        employee_2 = Employee(
+            first_name = duplicate_first_name,
+            last_name = 'Different_last_name',
+            job_title = JOB_TITLE,
+            job_description = JOB_DESCRIPTION,
+            skills = SKILLS,
+            bio = BIO
+        )
         employee_2.full_clean()
-        employee_3.full_clean()
+        employee_2.save()
+
+    def test_duplicate_last_name_different_first_name_is_valid(self):
+        duplicate_last_name = LAST_NAME
+
+        employee_1 = Employee(
+            first_name = FIRST_NAME,
+            last_name = duplicate_last_name,
+            job_title = JOB_TITLE,
+            job_description = JOB_DESCRIPTION,
+            skills = SKILLS,
+            bio = BIO
+        )
+        employee_1.full_clean()
+        employee_1.save()
+        employee_2 = Employee(
+            first_name = 'Different_first_name',
+            last_name = duplicate_last_name,
+            job_title = JOB_TITLE,
+            job_description = JOB_DESCRIPTION,
+            skills = SKILLS,
+            bio = BIO
+        )
+        employee_2.full_clean()
+        employee_2.save()
     
 
-    #Sqllite is not enforcing character constraints. Switch to postgres, then test.
+    
+class CharacterLimitTest(EmployeeModelTest):
+     #Tests show sqllite is not enforcing character constraints. Switch to postgres, then test again.
+
     @skip('')
-    def test_character_limits(self):       
-        employee_1 = Employee(first_name = '123456789012345678901', last_name = 'Phal', job_title = 'Agent', job_description = 'Tough guy.', skills='Fighting crime.', bio='Steven Frederic Seagal (/sɪˈɡɑːl/; born April 10, 1952) is an American actor, producer, screenwriter, director, martial artist, and musician who holds American, Russian, and Serbian citizenship.')
-        employee_2 = Employee(first_name = 'Aeqs', last_name = '123456789012345678901', job_title = 'Agent', job_description = 'Tough guy.', skills='Fighting crime.', bio='Steven Frederic Seagal (/sɪˈɡɑːl/; born April 10, 1952) is an American actor, producer, screenwriter, director, martial artist, and musician who holds American, Russian, and Serbian citizenship.')
-        employee_3 = Employee(first_name = 'New', last_name = 'Hat', job_title = '123456789012345678901', job_description = 'Tough guy.', skills='Fighting crime.', bio='Steven Frederic Seagal (/sɪˈɡɑːl/; born April 10, 1952) is an American actor, producer, screenwriter, director, martial artist, and musician who holds American, Russian, and Serbian citizenship.')
-        employee_4 = Employee(first_name = 'Noom', last_name = 'Ewws', job_title = 'Agent', job_description = '0123456789012345678901234567890123456789012345678901234567890', skills='Fighting crime.', bio='Steven Frederic Seagal (/sɪˈɡɑːl/; born April 10, 1952) is an American actor, producer, screenwriter, director, martial artist, and musician who holds American, Russian, and Serbian citizenship.')
-        employee_5 = Employee(first_name = 'FASew', last_name = 'Auat', job_title = 'Agent', job_description = 'Cart', skills='0123456789012345678901234567890123456789012345678901234567890012345678901234567890123456789012345678901234567890123456789001234567890123456789012345678901234567890123456789012345678900123456789012345678901234567890123456789012345678901234567890012345678901234567890123456789012345678901234567890123456789001234567890123456789012345678901234567890123456789012345678900123456789012345678901234567890123456789012345678901234567890012345678901234567890123456789012345678901234567890123456789001234567890123456789012345678901234567890123456789012345678900123456789012345678901234567890123456789012345678901234567890.', bio='reasonsa')
-        employee_6 = Employee(first_name = 'Hamo', last_name = 'Looe', job_title = 'Agent', job_description = 'Cart', skills='Fighting crime.', bio='0123456789012345678901234567890123456789012345678901234567890012345678901234567890123456789012345678901234567890123456789001234567890123456789012345678901234567890123456789012345678900123456789012345678901234567890123456789012345678901234567890012345678901234567890123456789012345678901234567890123456789001234567890123456789012345678901234567890123456789012345678900123456789012345678901234567890123456789012345678901234567890012345678901234567890123456789012345678901234567890123456789001234567890123456789012345678901234567890123456789012345678900123456789012345678901234567890123456789012345678901234567890')
+    def test_character_limits_on_first_name(self):       
+        employee = Employee(
+            first_name = 'This string is too long.',
+            last_name = LAST_NAME,
+            job_title = JOB_TITLE,
+            job_description = JOB_DESCRIPTION,
+            skills = SKILLS,
+            bio = BIO
+        )
 
         with self.assertRaises(ValidationError):
-            employee_1.full_clean()
-            employee_1.save()
+            employee.full_clean()
+            employee.save()
 
-            employee_2.full_clean()
+    @skip('')
+    def test_character_limits_on_last_name(self):
+        employee = Employee(
+            first_name = FIRST_NAME
+            last_name = 'This string is too long.',
+            job_title = JOB_TITLE,
+            job_description = JOB_DESCRIPTION,
+            skills = SKILLS,
+            bio = BIO
+        )
 
-            employee_3.full_clean()
+        with self.assertRaises(ValidationError):
+            employee.full_clean()
+            employee.save()
 
-            employee_4.full_clean()
+    @skip('')
+    def test_character_limits_on_job_title(self):
+        employee = Employee(
+            first_name = FIRST_NAME
+            last_name = LAST_NAME,
+            job_title = 'This string is too long.',
+            job_description = JOB_DESCRIPTION,
+            skills = SKILLS,
+            bio = BIO
+        )
 
+        with self.assertRaises(ValidationError):
+            employee.full_clean()
+            employee.save()
+
+    @skip('')
+    def test_character_limits_on_job_description(self):
+        employee = Employee(
+            first_name = FIRST_NAME
+            last_name = LAST_NAME,
+            job_title = JOB_TITLE,
+            job_description = ('This string is too long. This string is too long. This string is too long. This'
+            ' string is too long. This string is too long. This string is too long. This string is too long. This' 
+            ' string is too long. This string is too long. This string is too long. This string is too long.'),
+            skills = SKILLS,
+            bio = BIO
+        )
+
+        with self.assertRaises(ValidationError):
+            employee.full_clean()
+            employee.save()
+
+    @skip('')
+    def test_character_limits_on_skills(self):
+        employee = Employee(
+            first_name = FIRST_NAME
+            last_name = LAST_NAME,
+            job_title = JOB_TITLE,
+            job_description = JOB_DESCRIPTION,
+            skills = ('This string is too long. This string is too long. This string is too long. This string is'
+            ' too long. This string is too long. This string is too long. This string is too long. This string is'
+            ' too long. This string is too long. This string is too long. This string is too long.'),
+            bio = BIO
+        )
+
+        with self.assertRaises(ValidationError):
+            employee.full_clean()
+            employee.save()
+
+    @skip('')
+    def test_character_limits_on_bio(self):
+        employee = Employee(
+            first_name = FIRST_NAME,
+            last_name = LAST_NAME,
+            job_title = JOB_TITLE,
+            job_description = JOB_DESCRIPTION,
+            skills = SKILLS,
+            bio = ('This string is too long. This string is too long. This string is too long. This string is too'
+            ' long. This string is too long. This string is too long. This string is too long. This string is too'
+            ' long. This string is too long. This string is too long. This string is too long.')
+        )
+            
+        with self.assertRaises(ValidationError):
+            employee.full_clean()
+            employee.save()
 
